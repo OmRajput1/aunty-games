8564d144632536478c3a25ecb438b102d657b35b
Om Rajput <or.omrajput11@gmail.com>
2026-07-12 08:37:37 +0530

    audio added

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
-
+@onready var audio_stream_player_2: AudioStreamPlayer = $AudioStreamPlayer2
+@onready var audio_stream_player: AudioStreamPlayer = $"../../../AudioStreamPlayer"
 
 func _on_body_entered(body: Node2D) -> void:
 	print("u died")
+	audio_stream_player_2.play()
+	audio_stream_player.stop()
 	timer.start()
 
 func _on_timer_timeout() -> void:
